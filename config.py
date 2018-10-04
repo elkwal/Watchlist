6e7cda9844c7264f0441104ba63dc5cc6537e25e
Elukwal <elukwal3@gmail.com>
2018-10-04 09:14:17 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    '''
+    Production configuration  child class
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig
+'production':ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY =<c34426970c98797bf0d2167f0f66b5c9>
+export SECRET_KEY =<Flask WTF Secret Key>
+
+python3.7 manager.py server
